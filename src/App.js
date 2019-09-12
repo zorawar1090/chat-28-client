2e1cef512a516676977c8873f33d0edecdf588f8
zorawar1090 <zorawarsingh9988@gmail.com>
2019-09-12 20:49:18 +0200

    add routes

--- src/App.js
+++ src/App.js
@@ -1,51 +1,13 @@
-import React from 'react';
-import { allChannels } from './actions'
-import { connect } from 'react-redux'
-import MessageForm from './components/MessageForm'
-import UserForm from './components/UserForm'
-import ChannelForm from './components/ChannelForm'
+import React from 'react'
+import { Route } from 'react-router-dom'
+import ChannelList from './components/ChannelList'
+import Channel from './components/Channel'
 
-
-class App extends React.Component {
-  state = { message: '' }
-
-  source = new EventSource('https://safe-ocean-30227.herokuapp.com/stream')
-
-  componentDidMount() {
-    this.source.onmessage = (event) => {
-      const channels = JSON.parse(event.data)
-      this.props.allMessages(channels)
-    }
-  }
-
-  
-
-  render() {
-    const channels = this
-      .props
-      .channels
-      .map((channel, index) => <p key={index}>{channel.name}</p>)
-
-    
-
-    return <main>
-      <ChannelForm user={this.props.user} />
-      <UserForm user={this.props.user}/>
-      <MessageForm user={this.props.user} />
-      {channels}
-    </main>
+export default class App extends React.Component {
+  render () {
+    return <div>
+      <Route exact path='/' component={ChannelList} />
+      <Route path='/channel/:channelId' component={Channel} />
+    </div>
   }
 }
-
-const mapDispatchToProps = {
-  allChannels
-}
-
-const mapStateToProps = state => {
-  return {
-    channels: state.channels,
-    user: state.user
-  }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(App)
